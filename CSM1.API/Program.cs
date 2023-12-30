6234b64d913f0b9446124c2684e191c6e3f68091
Jafar <mi625nkm8.@code.edu.az>
2023-12-30 22:48:37 +0400

    *experementAttempt03

--- CSM1.API/Program.cs
+++ CSM1.API/Program.cs
@@ -7,7 +7,7 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
-        // testing holy power.3
+        // testing holy power.4
 
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
